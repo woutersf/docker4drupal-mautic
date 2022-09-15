53e59ed9575822fa27524870a2c505a20b75d749
Frederik Wouters <frederik.wouters@dropsolid.com>
2022-09-15 15:46:07 +0200

    added gitpod config to make it publicly available

---
+++ settings.php
@@ -0,0 +1,35 @@
+<?php
+
+$databases['default']['default'] = [
+    'host' => 'mariadb',
+    'database' => 'drupal',
+    'username' => 'drupal',
+    'password' => 'drupal',
+    'driver' => 'mysql',
+    'prefix' => '',
+];
+
+$settings['config_sync_directory'] = '/mnt/files/config/sync_dir';
+$settings['hash_salt'] = 'Z6AZA+gZXyd+vkAy';
+
+$settings['trusted_host_patterns'] = array(
+    '\.localhost$', '\.local$', '\.loc$',
+    '.*\.gitpod\.io'
+);$databases['default']['default'] = [
+    'host' => 'mariadb',
+    'database' => 'drupal',
+    'username' => 'drupal',
+    'password' => 'drupal',
+    'driver' => 'mysql',
+    'prefix' => '',
+];
+
+$settings['config_sync_directory'] = '/mnt/files/config/sync_dir';
+$settings['hash_salt'] = 'xsPY3cT/5Bqgt9dP';
+
+$settings['trusted_host_patterns'] = array(
+    '\.localhost$', '\.local$', '\.loc$', '\.gitpod\.io'
+);
+
+
+//ooFXkT23G5bXSTdsmAPC
